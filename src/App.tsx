07e329b2a5f48cad66374a9eaacfeceaa8bf6eb6
arielcorte <cortearielformal@gmail.com>
2023-08-04 16:19:21 -0300

    add title to main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet";
 import { io } from "socket.io-client";
 import { useEffect, useRef, useState } from "react";
 import QRCode from "react-qr-code";
@@ -80,59 +81,68 @@ function App() {
   }
 
   return (
-    <main className="bg-green-100 h-dscreen overflow-hidden flex flex-col">
-      <h1 className="text-3xl font-bold p-4 bg-green-400 text-white">
-        ChatsappAI
-      </h1>
-      <div className="flex flex-col gap-4 justify-center items-center grow p-8">
-        <p className="w-full max-w-sm">
-          Connection status:{" "}
-          <span className={cn(connected ? "text-green-500" : "text-red-500")}>
-            {connected ? "Connected" : "Disconnected"}
-          </span>
-        </p>
-        <div className="w-full max-w-sm">
-          <Label htmlFor="userId">User ID</Label>
-          <Input
-            id="userId"
-            placeholder="+549351..."
-            ref={userIdRef}
-            disabled={!connected}
-          />
-        </div>
-        <div className="flex gap-2">
-          <Button
-            onClick={() => createClient(userIdRef.current)}
-            disabled={!connected}
-          >
-            Create Client
-          </Button>
-          <Button
-            variant="outline"
-            onClick={() => deleteClient(userIdRef.current)}
-            disabled={!connected}
-          >
-            Delete Client
-          </Button>
-        </div>
-        <div className="flex items-center justify-center grow aspect-square bg-slate-50 rounded-sm border p-6">
-          {showQr ? (
-            qrCode ? (
-              <QRCode value={qrCode} className="w-full h-full" />
+    <>
+      <Helmet>
+        <title>ChatsappAI</title>
+        <meta
+          name="description"
+          content="ChatsappAI, a chatbot for whatsapp. By RevHouse."
+        />
+      </Helmet>
+      <main className="bg-green-100 h-dscreen overflow-hidden flex flex-col">
+        <h1 className="text-3xl font-bold p-4 bg-green-400 text-white">
+          ChatsappAI
+        </h1>
+        <div className="flex flex-col gap-4 justify-center items-center grow p-8">
+          <p className="w-full max-w-sm">
+            Connection status:{" "}
+            <span className={cn(connected ? "text-green-500" : "text-red-500")}>
+              {connected ? "Connected" : "Disconnected"}
+            </span>
+          </p>
+          <div className="w-full max-w-sm">
+            <Label htmlFor="userId">User ID</Label>
+            <Input
+              id="userId"
+              placeholder="+549351..."
+              ref={userIdRef}
+              disabled={!connected}
+            />
+          </div>
+          <div className="flex gap-2">
+            <Button
+              onClick={() => createClient(userIdRef.current)}
+              disabled={!connected}
+            >
+              Create Client
+            </Button>
+            <Button
+              variant="outline"
+              onClick={() => deleteClient(userIdRef.current)}
+              disabled={!connected}
+            >
+              Delete Client
+            </Button>
+          </div>
+          <div className="flex items-center justify-center grow aspect-square bg-slate-50 rounded-sm border p-6">
+            {showQr ? (
+              qrCode ? (
+                <QRCode value={qrCode} className="w-full h-full" />
+              ) : (
+                <div className="flex flex-col gap-2 w-full max-w-xs items-center">
+                  <div>Generating Qr...</div>
+                  <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+                </div>
+              )
+            ) : ready ? (
+              <div>Client ready!</div>
             ) : (
-              <div className="flex flex-col gap-2 w-full max-w-xs items-center">
-                <div>Generating Qr...</div>
-                <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
-              </div>
-            )
-          ) : ready ? (
-            <div>Client ready!</div>
-          ) : (
-            <div></div>
-          )}
+              <div></div>
+            )}
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
 
